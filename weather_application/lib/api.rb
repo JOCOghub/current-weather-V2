6759bef9aa8c1f2213817029e81399d67260ecd2
your name hereJoJo <Jojocohen57@gmail.com>
2020-07-28 11:55:14 -0400

    Changes city Id to city name

--- weather_application/lib/api.rb
+++ weather_application/lib/api.rb
@@ -1,4 +1,11 @@
-class Api
+class API
+  
+    @@api_key = "afabb8e5d2cb9bf2ff8f9db8da6bb07a"
 
+    def self.get_city_weather(city)
+        url = "https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{@@api_key}"
+        response = HTTParty.get(url)
+        binding.pry
+    end                 
    
 end   
--- weather_application/lib/cli.rb
+++ weather_application/lib/cli.rb
@@ -1,4 +1,33 @@
-class Cli
+class CLI
+    # def run
+    #     menu
+    #     goodbye
+        
+    # end     
+   
+    def run
+       #city = nil
+        #while city != "exit"
+            puts "Enter the City you would like info about or press exit."
+           city = gets.strip #make it not a city id!!!
+           self.get_weather_for_city(city)
+        #     case city 
+        #     when "city a"
+        #         puts "city a info"
+        #     when "city b"   
+        #         puts "city b info"
+        #     else 
+        #         puts "Please enter a valid city code" 
+        #     end 
+        # end         
+    end   
+    
+    def get_weather_for_city(city_id)
+        API.get_city_weather(city_id)
+    end     
 
-
-end   
+    def goodbye
+        puts "Come back for more weather updates."
+    end    
+    
+end  
