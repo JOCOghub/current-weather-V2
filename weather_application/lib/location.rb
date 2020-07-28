0f81e782bd9f86bdee736f9c823f5d06a41407e1
your name hereJoJo <Jojocohen57@gmail.com>
2020-07-28 14:04:32 -0400

    adds find method and class variable

--- weather_application/lib/location.rb
+++ weather_application/lib/location.rb
@@ -1,8 +1,17 @@
 class Location
     attr_accessor :name, :main, :description, :temperature, :feels_like, :high, :low
 
+    @@all = []
+
     def initialize(hash)
       hash.each do |k, v|
         self.send("#{k}=", v)
-    end    
+        end    
+      @@all << self 
+    end 
+
+   def find_by_name(name)
+      @@all.find{&:name}
+   end   
+
 end   
