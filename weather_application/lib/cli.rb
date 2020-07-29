cfb6be856a04be1f51e5814c1665c542f070d086
your name hereJoJo <Jojocohen57@gmail.com>
2020-07-28 22:42:30 -0400

    Adds menu, polished other methods, and set up show attributes method

--- weather_application/lib/cli.rb
+++ weather_application/lib/cli.rb
@@ -1,12 +1,12 @@
 class CLI
-    def run
-       menu
-       goodbye 
-    end     
+    # def run
+    #    welcome
+        
+    # end     
    
-    def menu
-        puts "Welocome to the City weather application App!"
-        puts "Enter the City you would like info about or press exit."
+    def welcome
+        puts "Welcome to the City weather application App!"
+        puts "Enter the City you would like weather info for or press exit."
         city = gets.strip 
         self.get_weather_for_city(city)
     end   
@@ -19,28 +19,42 @@ class CLI
          if valid?#Find out what would make this valid or not
             current_city = City.new(response)
          else 
-            puts "Please enter a valid City name"   
+            puts "Please enter a valid City name" 
+            self.get_weather_for_city  
          end 
        end 
-       self.prompt_user   
+       self.show_attributes
+    #    self.prompt_user   
     end     
     
-    def self.prompt_user
-        puts "Enter w to see the weather details for your selected city "
-        input = gets.strip 
-        if input = "w"
-            self.show_attributes
-        else 
-           self.prompt_user
-        end    
-    end
+    # def self.prompt_user
+    #     puts "Enter w to see the weather details for your selected city "
+    #     input = gets.strip 
+    #     if input = "w"
+    #         self.show_attributes
+    #     else 
+    #        self.prompt_user
+    #     end    
+    # end
     
     def self.show_attributes
         
+
+        self.menu #need self? ask govgov
     end   
 
-    def goodbye
-        puts "Come back for more weather updates."
-    end    
+    def menu
+       puts "Enter 1 to go to main menu or press `exit`"
+       input = gets.strip 
+       if input = "1"
+          self.welcome 
+       elsif input = "exit"   
+          abort("Come back for more weather updates")
+       else 
+        puts "invalid input"
+        menu   
+       end
+        self.goodbye 
+    end       
     
 end  
