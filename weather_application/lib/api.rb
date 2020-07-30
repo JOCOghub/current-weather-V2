b038d2635178bdbd79bcb312c5ce838a08ec2c32
your name hereJoJo <Jojocohen57@gmail.com>
2020-07-30 16:38:32 -0400

    Adds valid exit escape and fixes invalid_city_ name

--- weather_application/lib/api.rb
+++ weather_application/lib/api.rb
@@ -1,18 +1,19 @@
 class API
     @@api_key = "afabb8e5d2cb9bf2ff8f9db8da6bb07a"
-    attr_reader :current_city
+    #attr_reader :current_city
     def self.get_city_weather(input)
         url = "https://api.openweathermap.org/data/2.5/weather?q=#{input}&appid=#{@@api_key}"
         response = HTTParty.get(url)
         if !response["message"]
-            @current_city = City.new({name: response["name"], main: response["main"], description: response["description"],
+            current_city = City.new({name: response["name"], main: response["main"], description: response["description"],
             temperature: response["temp"], feels_like: response["feels_like"], high: response["temp_min"],
             low: response["temp_max"]}) #<-this is my constructor
+            return current_city
         else 
-            CLI.invalid_city_name
+            nil                    #CLI.invalid_city_name
         end
-        #CLI.get_weather_for_city(input)
-       CLI.show_attributes(@current_city) 
+                                #CLI.get_weather_for_city(input)
+                            #CLI.show_attributes(@current_city) 
     end                 
    
 end  
--- weather_application/lib/cli.rb
+++ weather_application/lib/cli.rb
@@ -1,43 +1,46 @@
 class CLI
-    # def run
-    #    welcome
-    #    API.get_city_weather(input)
-    #    get_weather_for_city(input)
-    #    show_attributes
-    #    menu
-    # end     
-    attr_accessor :current_city
     def welcome
-        puts "Welcome to the City weather application App!"
+        puts "Welcome to the City weather App!"
         puts "Enter the City you would like weather info for or press exit."
         input = gets.strip 
-        get_weather_for_city(input)
+        if input == "exit"
+          abort("Come back for more weather updates.")
+        else  
+          get_weather_for_city(input)
+        end   
     end   
     
     def get_weather_for_city(input)#this makes it that if user already asked for certain
        if City.find_by_name(input)#city then it would be stored and we wouldnt have
-          @current_city = City.find_by_name(input)#to look up API data again
-       else 
-          @current_city = API.get_city_weather(input)#might need API.new.get_city_weather
+          current_city = City.find_by_name(input)#to look up API data again
+          show_attributes(current_city)
+       else
+          current_city = API.get_city_weather(input)
+          show_attributes(current_city) if current_city
        end  
+        invalid_city_name
     end
-
-    def self.invalid_city_name
-        puts "Please enter a valid City's name"  
+   
+    def invalid_city_name
+        puts "Please enter a valid City's name or press `exit`"  
         input = gets.strip
-        API.get_city_weather(input)    
+        if input == "exit"
+            abort("Come back for more weather updates.")
+        else   
+            get_weather_for_city(input)
+        end  
     end       
 
-    def self.show_attributes(current_city)
+    def show_attributes(current_city)
         print current_city#how
         self.menu 
     end   
 
-    def self.menu
+    def menu
         puts "Enter 1 to go to main menu or press `exit`"
         input = gets.strip 
         if input == "1"
-           self.welcome #breaking code
+           self.welcome
         elsif input == "exit"   
            abort("Come back for more weather updates")
         else 
